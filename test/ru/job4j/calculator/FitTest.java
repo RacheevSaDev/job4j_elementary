42cc4a0135e95d795a8822c462d358e57c6a9f64
racheev <racheev.sa.dev@gmail.com>
2020-09-27 11:54:26 +0700

    2. Тесты для задачи "идеальный вес" [#370992]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenMan160Then69() {
+        short in = 160;
+        double expected = 69;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman160Then57p5() {
+        short in = 160;
+        double expected = 57.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman180Then80p5() {
+        short in = 180;
+        double expected = 80.5;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+}
