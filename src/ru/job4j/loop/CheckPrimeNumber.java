0c5dbbf4abe4b1f5f6e243dbb65c3aba4da735f6
racheev <racheev.sa.dev@gmail.com>
2020-10-02 23:04:18 +0700

    5.5. Простое число [#371018] Более адекватная предворительная проверка на то, что число не является простым

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,7 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        // по определению не являются простыми числами
-        if (number == 1 || number == 0) {
-            return false;
-        }
+        boolean prime = number > 1;
         // проверка на делимость. Достаточно проверять до корня из числа
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
